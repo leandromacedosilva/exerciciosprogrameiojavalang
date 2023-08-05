1feae8d8f80441e0648df04c2d71c50831dc5e86
leandromacedosilva <iamfulldeveloper@gmail.com>
2023-08-05 17:45:45 -0300

    file ControleRemoto.java upddated with success!

--- modelo/ControleRemoto.java
+++ modelo/ControleRemoto.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelo;
 
 /**
@@ -9,5 +6,56 @@ package modelo;
  * @author leandro
  */
 public class ControleRemoto {
+    private int canal;
+    private int volume;
+    private int configuracoes;
+    
+    // Construtor 
+    public ControleRemoto(){}
+    
+    // Métodos
+    public void aumentarVolume(int maisVolume){}
+    
+    public void diminuirVolume(int menosVolume){}
+    
+    public void controleConfiguracoes(int configuracao){
+        if(this.configuracoes == 1){
+            System.out.println("Sintonia de canais realizada com sucesso!");
+        }
+        if(this.configuracoes == 2){
+            System.out.println("Ajuste de cores realizada com sucesso!");
+        }
+        if(this.configuracoes == 3){
+            System.out.println("Seu dispositivo está conectado à Internet.");
+        }else{
+            System.out.println("Nenhuma opção escolhida.");
+        }
+    }
+    
+    // Getters and Setters
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public void setCanal(int canal) {
+        this.canal = canal;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public int getConfiguracoes() {
+        return configuracoes;
+    }
+
+    public void setConfiguracoes(int configuracoes) {
+        this.configuracoes = configuracoes;
+    }
     
 }
